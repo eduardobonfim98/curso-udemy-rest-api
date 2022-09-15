a7ab1bf1d8af54c43c60de4380671bb4daece6bb
Eduardo Siqueira Bonfim <eduardosb@ciandt.com>
2022-09-15 16:06:14 -0300

    Section 7

---
+++ curso-udemy-rest-api/src/main/java/br/com/eduardo/controllers/PersonController.java
@@ -0,0 +1,60 @@
+package br.com.eduardo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.eduardo.model.Person;
+import br.com.eduardo.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired //vai gerenciar a dependencia de um componente, no caso vamos fazer com um service
+	private PersonServices service;
+	///private PersonServices service = new PersonServices();
+	
+	@RequestMapping(method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) {
+		return service.findById(id);
+	}
+	//@RequestParam is used mainly for filtering purposes Lets say you want to get George Martin's book
+	//@PathVariable is used mainly for getting individual objects or piece of data Lets say you want to get a book by its id:
+	//@RequestBody is used mainly for saving object(s)(or piece of data) Lets say you want to add a book:
+
+	@RequestMapping(method=RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") String id) {
+		service.delete(id);
+	}
+}
